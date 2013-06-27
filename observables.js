9970fb1728085455404a1d4f20a976df643db19e
Sebastian <sebasporto@gmail.com>
2013-06-27 17:03:47 +1000

    better implemenation of observables

--- observables.js
+++ observables.js
@@ -6,16 +6,18 @@
 
 		init: function(element, options) {
 			this.state = new can.Observe({
-				makeEnabled: false,
-				bgLoading: false,
-				bgLoaded: false,
-				fgLoading: false,
-				fgLoaded: false,
-				making: false
+				loadingImage: false,
+				loadingText: false,
+				text: '',
+				image: '',
+				making: false,
+				download: ""
 			});
+
 			var template = can.view("#template", {
 				state: this.state
-			})
+			});
+
 			can.$(element).append(template);
 			this.bindEvents();
 		},
@@ -23,48 +25,82 @@
 		bindEvents: function() {
 			var self = this;
 			this.state.bind('change', function(ev, attr, how, newVal, oldVal) {
-				var allLoaded = this.attr('bgLoaded') && this.attr('fgLoaded');
-				//console.log('allLoaded', allLoaded);
-				this.attr('makeEnabled', allLoaded);
+				console.log('attr changed ' + attr)
+				if (attr === 'text' || attr === 'image') self.checkMake();
 			});
 		},
 
-		'.selectors button click': function(ele, ev) {
-			console.log('selector');
-			var kind = ele.data('kind');
-			var theme = ele.data('theme');
-			this.state.attr('making', false);
-			this.loadImg(kind, theme);
+		'.btn_text click': function(ele, ev) {
+			var self = this;
+		
+			this.state.attr('loadingText', true);
+			this.state.attr('text', '');
+			
+			var keyword = $('input[name=input_text]').val();
+
+			this.loadTextService(keyword, function (res) {
+				console.log("text retrieved " + res);
+				self.state.attr('loadingText', false);
+				self.state.attr('text', res);
+			});
 		},
 	
-		'.btn_make click': function(ele, ev) {
-			this.make();
-		},
-
-		loadImg: function(kind, theme) {
+		'.btn_image click': function(ele, ev) {
 			var self = this;
-			this.state.attr(kind + 'Loading', true);
-			this.state.attr(kind + 'Loaded', false);
+			
+			this.state.attr('loadingImage', true);
+			this.state.attr('image', '');
 
-			this.loadImageService(theme, function (res) {
-				self.state.attr(kind + 'Url', res);
-				self.state.attr(kind + 'Loading', false);
-				self.state.attr(kind + 'Loaded', true);
+			var keyword = $('input[name=input_image]').val();
+
+			this.loadImageService(keyword, function (res) {
+				console.log("image retrieved " + res);
+				self.state.attr('loadingImage', false);
+				self.state.attr('image', res);
 			});
-
 		},
 
-		// This is a simulated external service that
-		// gets a parameter
-		// and returns an image url
-		loadImageService: function (theme, cb) {
-			return setTimeout(function() {
-				cb("http://lorempixel.com/600/200/" + theme);
-			}, 1500);
+		checkMake: function () {
+			console.log('checkMake');
+			var allLoaded = this.state.attr('text') && this.state.attr('image');
+			if (allLoaded) {
+				this.make();
+			} else {
+				this.state.attr('making', false);
+				this.state.attr('download', '');
+			}
 		},
 
 		make: function() {
+			var self = this;
 			this.state.attr('making', true);
+			this.makeService(function (res) {
+				console.log('making done');
+				self.state.attr('making', false);
+				self.state.attr('download', res);
+			});
+		},
+
+		
+		// This is a simulated external services
+
+		loadTextService: function (keyword, cb) {
+			return setTimeout(function() {
+				// cb("http://lorempixel.com/600/200/" + keyword);
+				cb('Some tweet about ' + keyword);
+			}, 1500);
+		},
+
+		loadImageService: function (keyword, cb) {
+			return setTimeout(function() {
+				cb("http://lorempixel.com/500/200/");
+			}, 1500);
+		},
+
+		makeService: function (cb) {
+			return setTimeout(function() {
+				cb("http://link/to/meme");
+			}, 1500);
 		}
 	});
 
